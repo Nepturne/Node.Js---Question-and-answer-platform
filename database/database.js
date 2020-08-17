ecceb1e2eba3ba6db307de798264daf20ce4518e
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2020-08-17 14:16:12 -0300

    Alteração no processo de senha do banco de dados na configuração do ORM Sequelize

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-
-const connection = new Sequelize("guiaperguntas", "root", "99845993", {
+// Após informar o usuário e a senha do banco , será efetuada a conexão com o seu banco de dados MySQL.
+const connection = new Sequelize("guiaperguntas", "root", "informe_aqui_a_senha_do_seu_bancodedados_mysqlworkbench", {
   host: "localhost",
   dialect: "mysql",
 });
